b88cb369c8aa1936ae96a4897bf00b0757e7d309
splatude65 <liam@toth.ie>
2026-03-08 14:42:01 +0000

    dash update

--- Assets/Code/playerMovement.cs
+++ Assets/Code/playerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 using UnityEngine.Rendering.Universal;
 
@@ -10,6 +11,7 @@ public class playerMovement : MonoBehaviour
     [SerializeField] private float moveSpeed = 5f;
     private Rigidbody2D rb;
     private Vector2 moveInput;
+    private Vector2 moveDirection;
     private Animator animator;
 
     [Header("Dashing")]
@@ -29,11 +31,12 @@ public class playerMovement : MonoBehaviour
 
     void Update()
     {
-        rb.linearVelocity = moveInput * moveSpeed;
         if (isDashing)
         {
             return;
         }
+        rb.linearVelocity = moveInput * moveSpeed;
+        moveDirection = new Vector2(moveInput.x, moveInput.y);
     }
 
     public void Move(InputAction.CallbackContext context)
@@ -62,12 +65,13 @@ public class playerMovement : MonoBehaviour
 
     private IEnumerator DashCoroutine()
     {
+        Physics2D.IgnoreLayerCollision(7, 8, true);
         canDash = false;
         isDashing = true;
 
         trailRenderer.emitting = true;
 
-        rb.linearVelocity = new Vector2(rb.linearVelocity.x * dashSpeed, rb.linearVelocity.y * dashSpeed);
+        rb.linearVelocity = new Vector2(moveDirection.x * dashSpeed, moveDirection.y * dashSpeed);
 
         yield return new WaitForSeconds(dashDuration);
 
@@ -75,6 +79,7 @@ public class playerMovement : MonoBehaviour
 
         isDashing = false;
         trailRenderer.emitting = false;
+        Physics2D.IgnoreLayerCollision(7, 8, false);
 
         yield return new WaitForSeconds(dashCooldown);
         canDash = true;
