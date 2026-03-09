bca16da55dd334e12e13627cb7f608afbc3a6357
splatude65 <liam@toth.ie>
2026-03-09 13:39:20 +0000

    tilemap + mapbounds

---
+++ Assets/Code/MapTransition.cs
@@ -0,0 +1,52 @@
+using Unity.Cinemachine;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Assertions.Must;
+
+public class MapTransition : MonoBehaviour
+{
+    [SerializeField] PolygonCollider2D mapBoundary;
+    CinemachineConfiner2D confiner;
+    [SerializeField] Direction direction;
+    [SerializeField] float additivePos = 2;
+    
+    enum Direction {Up, Down, Left, Right};
+
+    [System.Obsolete]
+    private void Awake()
+    {
+        confiner = FindObjectOfType<CinemachineConfiner2D>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            confiner.BoundingShape2D = mapBoundary;
+            UpdatePlayerPosition(collision.gameObject);
+        }
+    }
+
+    private void UpdatePlayerPosition(GameObject player)
+    {
+        Vector3 newPos = player.transform.position;
+
+        switch (direction)
+        {
+            case Direction.Up:
+                newPos.y += additivePos;
+                break;
+            case Direction.Down:
+                newPos.y -= additivePos;
+                break;
+            case Direction.Left:
+                newPos.x += additivePos;
+                break;
+            case Direction.Right:
+                newPos.x -= additivePos;
+                break;
+        }
+
+        player.transform.position = newPos;
+    }
+}
